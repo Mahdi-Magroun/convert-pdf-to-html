094aae278df0457b02111d58eb7cbcbe25a1ac9f
Mahdi <mmahdiwebdeveloper@gmail.com>
2022-03-21 23:59:35 +0100

    version 1

--- src/public/index.php
+++ src/public/index.php
@@ -2,31 +2,17 @@
 require_once('class/ConvertFile.class.php');
 // if you are using composer, just use this
 include '/var/www/html/api/convertPdfHtml/vendor/autoload.php';
-?>
 
-<!DOCTYPE html>
-<html>
-<body>
 
-<form action="index.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-</body>
-</html>
-<?php
-
-if(isset($_POST['submit'])){
-
-$toFileUpload=array(
-  "file"=>$_FILES['fileToUpload'],
-  'uniqueName'=>true,
-  // serverDownloadDir it's the input for the converter 
-  "serverDownloadDir"=>'/var/www/html/api/convertPdfHtml/input/',
-  "htmlOutputDir"=>'/var/www/html/api/convertPdfHtml/output',
-  "allowedExtention"=>['pdf'],
+  $toFileUpload=array(
+    "file"=>$_FILES['fileToUpload'],
+    'uniqueName'=>true,
+   // serverDownloadDir it's the input for the converter 
+    "serverDownloadDir"=>'/var/www/html/api/convertPdfHtml/input/',
+    "htmlOutputDir"=>'/var/www/html/api/convertPdfHtml/output',
+   "allowedExtention"=>['pdf'],
   
 );
   $upload=new fileUpload($toFileUpload);
